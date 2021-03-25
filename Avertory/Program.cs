cfc1e4480a48e9958e1809b261a56102a2cf8e5b
dbeti <domagoj.beti@lemax.net>
2021-03-25 20:00:23 +0100

    Add docker support.

--- Avertory/Program.cs
+++ Avertory/Program.cs
@@ -1,11 +1,8 @@
+using DAL;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using Avertory.Extensions;
+using Microsoft.Extensions.Configuration;
 
 namespace Avertory
 {
@@ -13,13 +10,20 @@ namespace Avertory
 	{
 		public static void Main(string[] args)
 		{
-			CreateHostBuilder(args).Build().Run();
+			CreateHostBuilder(args).Build().MigrateDatabase<AvertoryDbContext>().Run();
 		}
 
 		public static IHostBuilder CreateHostBuilder(string[] args) =>
 			Host.CreateDefaultBuilder(args)
 				.ConfigureWebHostDefaults(webBuilder =>
 				{
+					webBuilder.ConfigureAppConfiguration((hostingContext, config) =>
+					{
+						var env = hostingContext.HostingEnvironment;
+						config.AddJsonFile("appsettings.json", optional: true, reloadOnChange: true)
+							.AddJsonFile($"appsettings.{env.EnvironmentName}.json", optional: true, reloadOnChange: true);
+						config.AddEnvironmentVariables();
+					});
 					webBuilder.UseStartup<Startup>();
 				});
 	}
